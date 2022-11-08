c3ca495cc74cc506350464032593cf8da6169df4
kate-lvlup <kate.yalanskaya@gmail.com>
2022-11-08 10:54:28 +0200

    Added files with homework task1-8

---
+++ taskFour.java
@@ -0,0 +1,10 @@
+// napisat' class kotoruy vuvodit v consol' segodnyashnjuu daty i vremya
+
+import java.util.Date;
+
+class taskFour {
+    public static void main(String[] args) {
+        Date valueDate = new Date();
+        System.out.println(valueDate);
+    }
+}
