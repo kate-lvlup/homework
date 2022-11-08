267bba28e848e973106e37841bf1dd04bc4ccb7e
kate-lvlup <kate.yalanskaya@gmail.com>
2022-11-08 18:03:00 +0200

    Transfered files task1-8 to folder lesson2

--- taskOne.java
+++
@@ -1,7 +0,0 @@
-// napisat' class kotoruy vuvodit v console pervuy element argymentov komandnou stroki (command line argument)
-
-class taskOne {
-    public static void main(String args[]) {
-        System.out.println(args[0]);
-    }
-}
