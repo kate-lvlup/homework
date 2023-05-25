3b5ec3493303dea44e277ec274dd6993bc75df0f
kate-lvlup <kate.yalanskaya@gmail.com>
2023-05-25 17:31:37 +0300

    recipe_web_app commit 1

---
+++ recipe_web_app/src/main/java/com/example/recipe_web_app/modal/IngredientRecipe.java
@@ -0,0 +1,42 @@
+package com.example.recipe_web_app.modal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Builder
+@Entity
+@Table(name = "ingredient_recipe")
+public class IngredientRecipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ingredient_recipe_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ingredient_id")
+    private Ingredients ingredient;
+
+    @ManyToOne
+    @JoinColumn(name = "recipe_id")
+    private Recipes recipe;
+}
---
+++ recipe_web_app/src/main/java/com/example/recipe_web_app/modal/IngredientType.java
@@ -0,0 +1,35 @@
+package com.example.recipe_web_app.modal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Builder
+@Entity
+@Table(name = "ingredient_type")
+public class IngredientType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ingredient_type_id")
+    private Long id;
+
+    @Column(name = "ingredient_type_name", unique = true, nullable = false)
+    private String name;
+}
