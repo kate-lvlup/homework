4edbf72e88962108ff04c88d10fe900a1e63f92b
kate-lvlup <kate.yalanskaya@gmail.com>
2023-06-17 15:44:05 +0300

    Inversion of Control(IoC) / Dependency Injection(DI)

---
+++ spring_core_app/src/main/java/org/example/MusicPlayer.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class MusicPlayer {
+    private Music music;
+
+    //IoC - here we inject the dependency
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public void playMusic(){
+        System.out.println("Playing" + music.getSong());
+    }
+}
---
+++ spring_core_app/src/main/java/org/example/TestSpring.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Music music = context.getBean("musicBean", Music.class);
+        //applied the architecture IoC
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
+
+        context.close();
+    }
+}
