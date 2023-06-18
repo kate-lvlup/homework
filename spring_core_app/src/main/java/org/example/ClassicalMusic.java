fad2f39a9bd14a42f2e0ce2bc845f8f6a50a2c20
kate-lvlup <kate.yalanskaya@gmail.com>
2023-06-18 18:47:00 +0300

    Annotation @Scope, @Value, @PostConstruct, @PreDestroy

--- spring_core_app/src/main/java/org/example/ClassicalMusic.java
+++ spring_core_app/src/main/java/org/example/ClassicalMusic.java
@@ -1,9 +1,25 @@
 package org.example;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-public class ClassicalMusic implements Music{
+//@Scope("singleton")
+//@Scope("prototype")
+public class ClassicalMusic implements Music {
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
