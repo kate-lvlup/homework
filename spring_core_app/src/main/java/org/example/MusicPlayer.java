1c8d6ebe1dc5b1a5ec8f20b8087562351ed5b3b2
kate-lvlup <kate.yalanskaya@gmail.com>
2023-06-18 17:43:35 +0300

    Annotation @Component

--- spring_core_app/src/main/java/org/example/MusicPlayer.java
+++ spring_core_app/src/main/java/org/example/MusicPlayer.java
@@ -2,34 +2,8 @@ package org.example;
 
 public class MusicPlayer {
     private Music music;
-    private String name;
-    private int volume;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    //IoC - here we inject the dependency
     public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public MusicPlayer() {
-    }
-
-    public void setMusic(Music music) {
         this.music = music;
     }
 
